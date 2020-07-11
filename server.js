de13fdaad1399f40fe39132f5e7ddb497f86b183
Connor McGarty <cmcgarty@uab.edu>
2020-07-11 12:48:04 -0500

    db config, User model, users api routes

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express = require('express');
+const connectDB = require('./config/db');
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+connectDB();
+
+// Init middleware
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => res.send('API Running'));
 
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
+// listen on port 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`)); 
